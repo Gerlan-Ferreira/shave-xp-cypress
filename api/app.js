a079f1b86d1f994d17365b88ad057ac1eee2e4f6
gerlan.ferreira <gerlan.ferr@gmail.com>
2023-03-28 21:56:03 -0300

    Aula 04 - API Helper - part 2 - criando endpoint de inserir usuario
    criando o endpoint de inserir usuarios diretamente no banco de dados na nossa api helper. No qual esse usuário terá sua senha criptografada. Bem como ajustamos para que os testes chamem esse endpoint para inserir o usuário em vez de usar a api do próprio sistema.

--- api/app.js
+++ api/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
 
 app.use(express.json()) //api vai trabalhar com a representação JSON
 
-const { deleteUser } = require('./db')
+const { deleteUser, insertUser } = require('./db')
 
 app.get('/welcome', function (req, res) {
     res.json({ message: 'Olá QAx' })
@@ -16,6 +17,27 @@ app.delete('/user/:email', async function (req, res) {
     await deleteUser(email)
 
     res.status(204).end()
+
+})
+
+app.post('/user', async function (req, res) {
+
+    const { name, email, password, is_shaver } = req.body
+
+    const hashPass = await bcrypt.hash(password, 8) //criptografando a senha
+
+    const user = {
+        name: name,
+        email: email,
+        password: hashPass,
+        is_shaver: is_shaver
+    }
+
+    console.log(user)
+
+    const id = await insertUser(user)
+
+    res.status(201).json({ user_id: id })
 
 })
 
--- api/db.js
+++ api/db.js
@@ -16,6 +16,20 @@ async function deleteUser(email) {
 
 }
 
+async function insertUser(user){
+    
+    const sql = 'INSERT INTO users (name, email, password, is_shaver) VALUES ($1, $2, $3, $4) returning id'
+
+    const data = [user.name, user.email, user.password, user.is_shaver]
+
+    const result = await pool.query(sql, data)
+
+    const {id} = result.rows[0] //pegando o retorno da query na primeira posição que é o ID do banco e guardando em um objeto de constante.
+
+    return id
+}
+
 module.exports = {
-    deleteUser
+    deleteUser,
+    insertUser
 }
